410526df6aa97ad8ce667ded81d005181c63b38f
balazs <petrobalazsmate@gmail.com>
2015-09-10 16:08:08 +0200

    üres opciók esetén se haljon már meg a történet :)

--- modul/ugyfel/library/Form/Edit/MapServices.php
+++ modul/ugyfel/library/Form/Edit/MapServices.php
@@ -1,40 +1,47 @@
 <?php
 namespace Uniweb\Module\Ugyfel\Library\Form\Edit;
-use Uniweb\Module\Ugyfel\Library\Form\Edit\IntermediateObjects\Service;
+
+use Traversable;
 use Uniweb\Library\Form\AbstractMap;
+use Uniweb\Module\Ugyfel\Library\Form\Edit\IntermediateObjects\Service;
+use Uniweb\Module\Ugyfel\Model\ActiveRecord\ServiceInterested;
+use Uniweb\Module\Szolgaltatas\Model\ActiveRecord\Service as ServiceModel;
 
 class MapServices extends AbstractMap
 {
     public function map()
     {
         $intermediates = array();
-        /* @var $option \Uniweb\Module\Szolgaltatas\Model\ActiveRecord\Service */
-        foreach ($this->options as $option) {
-            $selected = null;
-            /* @var $selectedOption \Uniweb\Module\Ugyfel\Model\ActiveRecord\ServiceInterested */
-            foreach ($this->selectedOptions as $selectedOption) {                
-                $service = $selectedOption->service;
-                if ($service && $service->id == $option->id) {
-                    $selected = $selectedOption;
-                    break;
+        
+        if (is_array($this->options) || $this->options instanceof Traversable) {
+            /* @var $option ServiceModel */
+            foreach ($this->options as $option) {
+                $selected = null;
+                /* @var $selectedOption ServiceInterested */
+                foreach ($this->selectedOptions as $selectedOption) {                
+                    $service = $selectedOption->service;
+                    if ($service && $service->id == $option->id) {
+                        $selected = $selectedOption;
+                        break;
+                    } else {
+                        $selected = null;
+                    }
                 }
-                //} else {
-                //    $selected = null;
-                //}
+                /* @var $selected ServiceInterested */
+                $isSelected = !is_null($selected);
+                $intermediates[] = new Service(
+                    $isSelected ? $selected->ugyfel_attr_szolgaltatas_erdekelt_id : null,
+                    $option->szolgaltatas_id, 
+                    $option->nev, 
+                    $isSelected, 
+                    $isSelected ? $selected->reszt_akar_venni : null, 
+                    $isSelected ? $selected->reszt_vett : null, 
+                    $isSelected ? $selected->mikor : null,
+                    $selected
+                );
             }
-            /* @var $selected \Uniweb\Module\Ugyfel\Model\ActiveRecord\ServiceInterested */
-            $isSelected = !is_null($selected);
-            $intermediates[] = new Service(
-                $isSelected ? $selected->ugyfel_attr_szolgaltatas_erdekelt_id : null,
-                $option->szolgaltatas_id, 
-                $option->nev, 
-                $isSelected, 
-                $isSelected ? $selected->reszt_akar_venni : null, 
-                $isSelected ? $selected->reszt_vett : null, 
-                $isSelected ? $selected->mikor : null,
-                $selected
-            );
         }
+        
         return $intermediates;
     }
 }
